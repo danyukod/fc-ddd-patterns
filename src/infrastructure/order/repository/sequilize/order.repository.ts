ced373eefda7cbd19b5181dca4e0e57ef0bde722
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2023-03-13 19:49:49 -0300

    feat: removendo include de order.repository update.

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -42,9 +42,7 @@ export default class OrderRepository {
                 })),
             },
             {
-                where: {id: entity.id},
-                include: [{model: OrderItemModel}],
-            }
+                where: {id: entity.id}}
         );
     }
 
